cc4aefbe5f436146723233f60e17dcb859ce7614
erinhurlburt <77364422+erinhurlburt@users.noreply.github.com>
2024-02-07 17:07:49 -0800

    Adding search component and receives movies based on title and search key;possibly separate home page and search page

--- flickpick/src/App.js
+++ flickpick/src/App.js
@@ -4,10 +4,50 @@ import { HomeView } from "./Views/HomeView";
 import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Batman");
+
+  useEffect(() => {
+    getMovies();
+  }, [query]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://www.omdbapi.com/?s=${query}&apikey=dd43f597`
+    );
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data.Search);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
   return (
     <div className="App" id="App">
-      <header>Welcome to FlickPick!</header>
-      <HomeView />
+      <h2>Welcome to FlickPick!</h2>
+      <form onSubmit={getSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={updateSearch}
+          placeholder="Search for a movie..."
+        />
+        <button type="submit">Search</button>
+      </form>
+      {movies.map((movie) => (
+        <div key={movie.imdbID}>
+          <h3>{movie.Title}</h3>
+          <img src={movie.Poster} alt={movie.Title} />
+        </div>
+      ))}
     </div>
   );
 }
